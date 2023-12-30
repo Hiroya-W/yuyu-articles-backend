e08025edb8457a9cf17d7c94702585dc8758cc2c
Hiroya-W <hyuyukun@gmail.com>
2023-12-30 13:37:08 +0900

    :sparkles: PSR17で作ったResponseをEmitterで返す

--- public/index.php
+++ public/index.php
@@ -2,4 +2,12 @@
 
 declare(strict_types=1);
 
-echo "Hello World";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use \Nyholm\Psr7\Factory\Psr17Factory;
+
+$psr17Factory = new Psr17Factory();
+
+$responseBody = $psr17Factory->createStream('Hello world');
+$response = $psr17Factory->createResponse(200)->withBody($responseBody);
+(new \Laminas\HttpHandlerRunner\Emitter\SapiEmitter())->emit($response);
